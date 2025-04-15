17e3ccd2c3e121c243181c0583f7939978d8db8d
Thalia Danielle <thaliadani2@gmail.com>
2025-04-15 11:03:09 -0300

    Add files via upload

---
+++ assets/assets/src/main.js
@@ -0,0 +1,13 @@
+function abrirMenu() {
+    let botaoDoMenu = document.getElementById("btn-menu")
+    const links = document.querySelector('.links');
+
+    if(){
+        links.style.display = 'flex';
+
+    } else{
+        links.style.display = 'none';
+    }
+}
+
+
